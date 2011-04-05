8d2c720aa13e4014c1d17c70ec1950d7e6e71ee3
Hugo Stefan Kaus Puhlmann <hugopuhlmann@gmail.com>
2011-04-05 15:16:40 -0300

    initial commit

---
+++ src/Receiver.java
@@ -0,0 +1,141 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+import jade.core.Agent;
+import jade.core.behaviours.Behaviour;
+import jade.domain.DFService;
+import jade.domain.FIPAAgentManagement.DFAgentDescription;
+import jade.domain.FIPAAgentManagement.ServiceDescription;
+import jade.domain.FIPAException;
+import jade.lang.acl.ACLMessage;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import java.util.TreeMap;
+
+/**
+ *
+ * @author hstefan
+ */
+public class Receiver extends Agent {
+
+    /**
+     * @param args the command line arguments
+     */
+    public void setup() {
+	System.out.println("Starting Receiver Agent.");
+	System.out.println("Agent name: " + this.getAID().getName());
+	addBehaviour(new WaitingMessage(this));
+
+	DFAgentDescription dfd = new DFAgentDescription();
+        dfd.setName(getAID());
+        ServiceDescription sd = new ServiceDescription();
+        sd.setType("receiver");
+        sd.setName("dark-hole-file-transfer");
+        dfd.addServices(sd);
+        try {
+            DFService.register(this, dfd);
+        }
+        catch(FIPAException fe) {
+            System.out.println("Impossible to register this agent in the yellow pages");
+        }
+    }
+
+    public void takeDown() {
+	try {
+            DFService.deregister(this);
+        }
+        catch (FIPAException fe) {
+            System.out.println("Couldn't remove this agent from the yellow pages.");
+        }
+    }
+
+    class WaitingMessage extends Behaviour {
+
+	public WaitingMessage(Agent agent) {
+	    super(agent);
+	    don = false;
+	}
+
+	@Override
+	public void action() {
+	    ACLMessage msg = receive();
+	    if (msg != null) {
+		String param = msg.getUserDefinedParameter("start");
+		if (param != null && param.equals("true")) {
+		    String filepath = msg.getUserDefinedParameter("filepath");
+		    myAgent.addBehaviour(new ReceivingMessage(myAgent, filepath));
+		    myAgent.removeBehaviour(this);
+		    don = true;
+		}
+	    }
+	}
+
+	@Override
+	public boolean done() {
+	    return don;
+	}
+
+	private boolean don;
+    }
+
+    class ReceivingMessage extends Behaviour {
+	public ReceivingMessage(Agent agent, String filepath) {
+	    super(agent);
+	    try {
+		files = new TreeMap<String, FileOutputStream>();
+		FileOutputStream fos = new FileOutputStream(filepath);
+		files.put(filepath, fos);
+	    } catch (FileNotFoundException ex) {
+		Logger.getLogger(Receiver.class.getName()).log(Level.SEVERE, null, ex);
+	    }
+	}
+
+	@Override
+	public void action() {
+	   ACLMessage msg = receive();
+	   if (msg != null) {
+	       String op = msg.getUserDefinedParameter("stop");
+	       String filename = msg.getUserDefinedParameter("filepath");
+	       if(op != null && op.equals("true")) {
+		   System.out.println("Finished writting!");
+		   FileOutputStream fos = files.get(filename);
+		   if(fos != null) {
+			try {
+			    fos.close();
+			} catch (IOException ex) {
+			    Logger.getLogger(Receiver.class.getName()).log(Level.SEVERE, null, ex);
+			}
+			files.remove(filename);
+			myAgent.removeBehaviour(this);
+			myAgent.addBehaviour(new WaitingMessage(myAgent));
+			return;
+		   }
+	       }
+	       else if(filename != null) {
+		   FileOutputStream fos = files.get(filename);
+		   if (fos != null) {
+		       try {
+			   fos.write(msg.getContent().getBytes());
+			   System.out.println("Writting to");
+		       } catch (IOException ex) {
+			   System.err.println("Unable to write file.");
+			   Logger.getLogger(Receiver.class.getName()).log(Level.SEVERE, null, ex);
+		       }
+		   }
+	       }
+	   }
+	}
+
+	@Override
+	public boolean done() {
+	    return files.isEmpty();
+	}
+
+	private TreeMap<String, FileOutputStream> files;
+    }
+}
---
+++ src/Sender.java
@@ -0,0 +1,99 @@
+
+import jade.core.Agent;
+import jade.domain.DFService;
+import jade.domain.FIPAAgentManagement.DFAgentDescription;
+import jade.domain.FIPAAgentManagement.ServiceDescription;
+import jade.domain.FIPAException;
+import jade.lang.acl.ACLMessage;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.swing.ImageIcon;
+import javax.swing.JPanel;
+import javax.swing.JFrame;
+import javax.swing.JScrollPane;
+import remotepi.FileDrop;
+import remotepi.ImagePanel;
+
+public class Sender extends Agent {
+    @Override
+    public void setup()
+    {        
+	JFrame frame = new JFrame("Black Hole File Transferer");
+	JPanel panel = new JPanel();
+	ImagePanel img_pan = new ImagePanel(new ImageIcon("images/background.png").getImage());
+
+	frame.getContentPane().add(img_pan);
+        frame.getContentPane().add(new JScrollPane( panel ),
+            java.awt.BorderLayout.CENTER );
+        new FileDrop( System.out, img_pan, /*dragBorder,*/ new FileDrop.Listener() {
+	    public void filesDropped( File[] files ) {
+		DFAgentDescription[] result = getAgents();
+		for(File f : files) {
+		    sendFile(f, result);
+		}
+            }
+        });
+	
+	frame.pack();
+	frame.setBounds(100, 100, (int)img_pan.getDimension().getWidth(),
+		(int)img_pan.getDimension().getHeight());
+        frame.setDefaultCloseOperation( JFrame.EXIT_ON_CLOSE );
+        frame.setVisible(true);
+    } 
+
+    public DFAgentDescription[] getAgents() {
+	DFAgentDescription template = new DFAgentDescription();
+	ServiceDescription sd = new ServiceDescription();
+	sd.setType("receiver");
+	template.addServices(sd);
+	try {
+	    DFAgentDescription[] result = DFService.search(this, null);
+	    return result;
+	} catch (FIPAException ex) {
+	    Logger.getLogger(Sender.class.getName()).log(Level.SEVERE, null, ex);
+	}
+	return null;
+    }
+    
+    public void sendFile(File file, DFAgentDescription[] agts) {
+	ACLMessage msg = new ACLMessage(ACLMessage.INFORM);
+
+	msg.addUserDefinedParameter("start", "true");
+	msg.addUserDefinedParameter("filepath", file.getName());
+
+	for(DFAgentDescription ag : agts) {
+	    msg.addReceiver(ag.getName());
+	}
+	send(msg);
+	try {
+	    FileInputStream fis = new FileInputStream(file);
+	    byte[] filecontent = new byte[4096];
+	    int bytes = -1;
+	    try {
+		msg.removeUserDefinedParameter("start");
+		while(true) {
+		   bytes = fis.read(filecontent);
+		   if(bytes <= 0) {
+		       msg.addUserDefinedParameter("stop", "true");
+		       System.out.println("Transmission concluded.");
+                       fis.close();
+		       break;
+		   } else {
+                        String f = new String(filecontent);
+			msg.setContent(f);
+			send(msg);
+		   }
+		}
+	    } catch (IOException ex) {
+		Logger.getLogger(Sender.class.getName()).log(Level.SEVERE, null, ex);
+	    }
+	} catch (FileNotFoundException ex) {
+	    Logger.getLogger(Sender.class.getName()).log(Level.SEVERE, null, ex);
+	}
+
+    }
+}
