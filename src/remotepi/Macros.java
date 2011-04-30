233cc1784b5d84107836b0fa93f5b388de799233
Hugo Puhlmann <hugopuhlmann@gmail.com>
2011-04-30 15:56:13 -0300

    macros that defines user param variables

---
+++ src/remotepi/Macros.java
@@ -0,0 +1,18 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package remotepi;
+
+/**
+ *
+ * @author hstefan
+ */
+public class Macros {
+    public static String RECEIVER_TYPE = "receiver-agent";
+    public static String SENDER_AGENT = "sender-agent";
+    public static String T_REQUEST_PARAM_NAME = "transmission-request";
+    public static String START_REQUEST = "start-transmission";
+    public static String STOP_REQUEST = "stop-transmission";
+    public static String FILENAME_PARAM = "filepath";
+}
